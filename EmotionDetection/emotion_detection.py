c7b3cf9545fd34eae8feef439c88eaf9b47b9d8b
JoshuaAuf <aufderheide.j@gmail.com>
2025-03-05 15:11:37 -0500

    error handling complete

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,22 +7,41 @@ def emotion_detector(text_to_analyse):  # Define a function named sentiment_anal
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    data = json.loads(response.text) # Load the JSON response into a Python dictionary
-    emotion_predictions = data["emotionPredictions"] # Extract the emotion predictions
-    emotion_scores = emotion_predictions[0]["emotion"] # Extract the emotion scores
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get) # Determine the dominant emotion
     
-    formatted_response = "For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}.".format(**output)
-
-    # Create the output dictionary
+    try:
+        data = json.loads(response.text) # Load the JSON response into a Python dictionary
+        emotion_predictions = data["emotionPredictions"] # Extract the emotion predictions
+        emotion_scores = emotion_predictions[0]["emotion"] # Extract the emotion scores
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get) # Determine the dominant emotion
+    
+        # Create the output dictionary
         output = {
-            "anger": emotion_scores["anger"],
-            "disgust": emotion_scores["disgust"],
-            "fear": emotion_scores["fear"],
-            "joy": emotion_scores["joy"],
-            "sadness": emotion_scores["sadness"],
-            "dominant_emotion": dominant_emotion
+        "anger": emotion_scores["anger"],
+        "disgust": emotion_scores["disgust"],
+        "fear": emotion_scores["fear"],
+        "joy": emotion_scores["joy"],
+        "sadness": emotion_scores["sadness"],
+        "dominant_emotion": dominant_emotion
         }    
+        
+        if response.status_code == 400:
+           output = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        } 
+
+    except Exception:
+        output = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
     
-    return formatted_response 
-   
+    return output
